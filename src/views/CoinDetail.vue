2f5310cdfa80db67dbff4d92a8f4039d7a4cd7c1
anbreaker <antunez19@gmail.com>
2021-06-27 11:57:55 +0200

    final project

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -61,6 +61,7 @@
 
         <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
           <button
+            @click="toggleConverter"
             class="
               bg-green-500
               hover:bg-green-700
@@ -71,12 +72,13 @@
               rounded
             "
           >
-            Cambiar
+            {{ fromUsd ? `USD a ${asset.symbol}` : `${asset.symbol} a USD` }}
           </button>
 
           <div class="flex flex-row my-5">
             <label class="w-full" for="convertValue">
               <input
+                v-model="convertValue"
                 id="convertValue"
                 type="number"
                 class="
@@ -92,11 +94,14 @@
                   appearance-none
                   leading-normal
                 "
+                :placeholder="`Valor en ${fromUsd ? 'USD' : asset.symbol}`"
               />
             </label>
           </div>
 
-          <span class="text-xl"></span>
+          <span class="text-xl">
+            {{ convertResult }} {{ fromUsd ? asset.symbol : 'USD' }}
+          </span>
         </div>
       </div>
 
@@ -107,25 +112,68 @@
         :max="max"
         :data="history.map((h) => [h.date, parseFloat(h.priceUsd).toFixed(2)])"
       />
+
+      <h3 class="text-xl my-10">Mejores Ofertas de Cambio</h3>
+      <table>
+        <tr
+          v-for="m in markets"
+          :key="`${m.exchangeId}-${m.priceUsd}`"
+          class="border-b"
+        >
+          <td>
+            <b>{{ m.exchangeId }}</b>
+          </td>
+          <td>{{ m.priceUsd | dollar }}</td>
+          <td>{{ m.baseSymbol }} / {{ m.quoteSymbol }}</td>
+          <td>
+            <px-button
+              :is-loading="m.isLoading || false"
+              v-if="!m.url"
+              @custom-click="getWebSite(m)"
+            >
+              <slot>Obtener Link</slot>
+            </px-button>
+            <a v-else class="hover:underline text-green-600" target="_blanck">{{
+              m.url
+            }}</a>
+          </td>
+        </tr>
+      </table>
     </template>
   </div>
 </template>
 
 <script>
 import api from '../api';
+import PxButton from '../components/PxButton.vue';
 
 export default {
   name: 'CoinDetail',
+
+  components: { PxButton },
 
   data() {
     return {
       isLoading: false,
       asset: {},
       history: [],
+      markets: [],
+      fromUsd: true,
+      convertValue: null,
     };
   },
 
   computed: {
+    convertResult() {
+      if (!this.convertValue) return 0;
+
+      const result = this.fromUsd
+        ? this.convertValue / this.asset.priceUsd
+        : this.convertValue * this.asset.priceUsd;
+
+      return result.toFixed(4);
+    },
+
     min() {
       return Math.min(
         ...this.history.map((h) => parseFloat(h.priceUsd).toFixed(2))
@@ -145,11 +193,21 @@ export default {
     },
   },
 
+  watch: {
+    $route() {
+      this.getCoin();
+    },
+  },
+
   created() {
     this.getCoin();
   },
 
   methods: {
+    toggleConverter() {
+      this.fromUsd = !this.fromUsd;
+    },
+
     async getCoin() {
       const id = this.$route.params.id;
 
@@ -160,8 +218,26 @@ export default {
       Promise.all([
         (this.asset = await api.getAsset(id)),
         (this.history = await api.getAssetHistory(id)),
+        (this.markets = await api.getMarkets(id)),
       ]).finally(() => (this.isLoading = false));
+    },
+
+    async getWebSite(exchange) {
+      this.$set(exchange, 'isLoading', true);
+
+      const response = await api.getExchange(exchange.exchangeId);
+
+      // exchange.url = response.exchangeUrl; // Solucion a problema Reactividad VueJs2
+      this.$set(exchange, 'url', response.exchangeUrl);
+      this.$set(exchange, 'isLoading', false);
     },
   },
 };
 </script>
+
+<style scoped>
+td {
+  padding: 10px;
+  text-align: center;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: '/coin-vuejs-cli',
-}
+  publicPath: '/',
+};
